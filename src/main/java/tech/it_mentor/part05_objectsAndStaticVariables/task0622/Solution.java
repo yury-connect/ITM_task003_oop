1f54a2b832222c4e37d2060a9fee0abeb760bde5
Yury <yury.connect@gmail.com>
2024-09-01 23:54:17 +0300

    solutions: task0622

--- src/main/java/tech/it_mentor/part05_objectsAndStaticVariables/task0622/Solution.java
+++ src/main/java/tech/it_mentor/part05_objectsAndStaticVariables/task0622/Solution.java
@@ -2,6 +2,7 @@ package tech.it_mentor.part05_objectsAndStaticVariables.task0622;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* 
 Числа по возрастанию
@@ -33,6 +34,24 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //напишите тут ваш код
+        final int size = 5;
+
+        int[] src = new int[size]; // Использую сортировку пузырьком, т.к. в рамках текущей задачи асимптотика не важна
+        for (int i = 0; i < size; i++) {
+            System.out.printf("Введите элемент № '%d' из '%d'", i + 1, size);
+            src[i] = Integer.parseInt(reader.readLine());
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (src[j] > src[i]) {
+                    int tmp = src[i];
+                    src[i] = src[j];
+                    src[j] = tmp;
+                }
+            }
+        }
+
+        Arrays.stream(src).forEach(i -> System.out.println(i + " "));
     }
 }
